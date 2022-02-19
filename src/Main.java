f8fe310246ea044ccd736033d8c00817a9e658c6
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2022-02-19 12:19:28 -0700

    Fixing Mistakes

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,30 @@
 public class Main {
     public static void main(String args[]) {
-        //Project2 R1 = new Project2( "Thread 1");
-        //R1.start();
-
-        //Project2 R2 = new Project2( "Thread 2");
-        //R2.start();
 
 
 
-        Project2 R3 = new Project2("Thread ");
+
+
+
+          Project2 R1 = new Project2( "Thread 1");
+          R1.start();
+          R1.timeThreads();
+//
+//
+//        Project2 R2 = new Project2( "Thread 2");
+//        R2.start();
+//
+//
+//
+//        Project2 R3 = new Project2("Thread ");
 
         //R3.createThreads();
         //R3.destroyingThreads();
         //R3.timeThreads();
         //R3.threadCalculations();
 
-        Project2 R4 = new Project2("Thread ");
-        R4.parentAndChildThreads();
+//        Project2 R4 = new Project2("Thread ");
+//        R4.parentAndChildThreads();
 
 
 
--- src/Project2.java
+++ src/Project2.java
@@ -7,8 +7,7 @@ public class Project2 implements Runnable {
     int threadCounter = 0;
     int destroyedThreadCounter = 0;
     //int threadChildCounter = 0;
-    List<Integer> threads = new ArrayList<>();
-    List<Integer> childThreads = new ArrayList<>();
+
 
 
 
@@ -21,10 +20,23 @@ public class Project2 implements Runnable {
     public void run() {
         System.out.println("Running " +  threadName );
         try {
-            for(int i = 4; i > 0; i--) {
+            // creating new threads
+//            for(int i = 0; i <=99 ; i++) {
+//                System.out.println("Thread: " + threadName + ", " + i);
+//                // Let the thread sleep for a while.
+//                Thread.sleep(50);
+//            }
+            // creating new threads starting from 1 to 100,000
+            for(int i=1; i<=100; i++)
+            {
                 System.out.println("Thread: " + threadName + ", " + i);
-                // Let the thread sleep for a while.
                 Thread.sleep(50);
+            }
+            // after threads have been created they are being destroyed in reverse order
+            System.out.println("Destroying Threads");
+            for(int i=100; i>=1; i--)
+            {
+                System.out.println("Thread: " + threadName + ", " + i);
             }
         } catch (InterruptedException e) {
             System.out.println("Thread " +  threadName + " interrupted.");
@@ -32,7 +44,9 @@ public class Project2 implements Runnable {
         System.out.println("Thread " +  threadName + " exiting.");
     }
 
-    public void start () {
+
+
+    public void start() {
         System.out.println("Starting " +  threadName );
         if (t == null) {
             t = new Thread (this, threadName);
@@ -40,94 +54,26 @@ public class Project2 implements Runnable {
         }
     }
 
-    public void createThreads(){
-        System.out.println("Creating 100,000 threads");
-        for(int i=0; i<=99999; i++)
-        {
-            System.out.println("Creating " + threadName + i);
-            threadCounter +=1;
-            threads.add(i);
-        }
-    }
-
-    public void destroyingThreads(){
-        System.out.println("Destroying 100,000 threads");
-        {
-            for(int i=99999; i>0; i--)
-            {
-                System.out.println("Destroying " + threadName + i);
-                destroyedThreadCounter +=1;
-            }
-        }
-    }
-
     public void timeThreads(){
         //https://stackoverflow.com/questions/180158/how-do-i-time-a-methods-execution-in-java for the timing code
         long startTime = System.nanoTime();
-        createThreads();
+        run();
         long endTime = System.nanoTime();
         long duration = (endTime - startTime)/1000000 ;
-        System.out.println("Time to create 100 threads: " + duration + " milliseconds");
+        System.out.println("Time to create 100,000 threads: " + duration + " milliseconds");
         System.out.println();
         System.out.println();
         System.out.println();
 
         startTime = System.nanoTime();
-        destroyingThreads();
+        run();
         endTime = System.nanoTime();
         duration = (endTime - startTime)/1000000 ;
-        System.out.println("Time to destroy 100 threads: " + duration + " milliseconds");
+        System.out.println("Time to destroy 100,000 threads: " + duration + " milliseconds");
 
     }
 
-    public void threadCalculations(){
-        System.out.println("Threads Created: " + threadCounter);
-        System.out.println("Threads Destroyed: " + destroyedThreadCounter);
-    }
 
-    // Question 2 part b
-    public void parentAndChildThreads(){
-
-        System.out.println("Creating 100 threads " + threadName);
-        for(int i=0; i<=99; i++)
-        {
-            System.out.println("Creating " + threadName + i);
-            threadCounter +=1;
-            threads.add(i);
-        }
-        for(int threadName: threads)
-        {
-            System.out.println();
-            System.out.println("Thread: " + threadName);
-            for(int j=0; j<1000; j++)
-            {
-                System.out.println("Creating child #: "+ j+ " of thread: " + threadName);
-                childThreads.add(j);
-
-            }
-        }
-
-        // trying to delete all the child threads after they have been created
-        for(int threadName: threads)
-        {
-            System.out.println("Thread: " + threadName);
-            for(int j=999; j>0; j--)
-            {
-                System.out.println("deleting child #: "+ j+ " of thread: " + threadName);
-                childThreads.remove(j);
-            }
-        }
-        // Destroying all the parents in reverse order after all of the children have been destroyed.
-        System.out.println("Destroying 100 threads");
-        {
-            for(int i=99; i>0; i--)
-            {
-                System.out.println("Destroying " + threadName + i);
-                threads.remove(i);
-            }
-        }
-
-    }
 }
 
 
